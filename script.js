b8a23a5c5026fb98c32bda3f2ae70c95dad5271a
courtlandmessam <messamcourtland@gmail.com>
2020-01-13 20:47:49 -0500

    Added the ability to press keyboard keys to activate corresponding piano keys

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
+const WHITE_KEYS = ["z", "x", "c", "v", "b", "n", "m"];
+const BLACK_KEYS = ["s", "d", "g", "h", "j"];
+
 const keys = document.querySelectorAll('.key');
+const whiteKeys = document.querySelectorAll('.key.white');
+const blackKeys = document.querySelectorAll('.key.black');
+
 
 keys.forEach(key => {
   key.addEventListener('click', () => playNote(key));
 });
+
+document.addEventListener('keydown', e => {
+  const key = e.key;
+  if (e.repeat) return
+
+  const whiteKeysIndex = WHITE_KEYS.indexOf(key);
+  const blackKeysIndex = BLACK_KEYS.indexOf(key);
+
+  if (whiteKeysIndex > - 1) playNote(whiteKeys[whiteKeysIndex]);
+  if (blackKeysIndex > - 1) playNote(blackKeys[blackKeysIndex]);
+})
 
 function playNote(key) {
   const noteAudio = document.getElementById(key.dataset.note);
